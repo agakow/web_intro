70566cbfc9e2add73c02e531f9e9053c4ca3d7a6
Aga Kowalczuk <kowalczuk.a@outlook.com>
2016-07-18 14:38:33 +0100

    Updating app.rb and adding shotgun

--- app.rb
+++ app.rb
@@ -1,5 +1,19 @@
 require 'sinatra'
 
+set :session_secret, 'super secret'
+
 get '/' do
   "Hello World"
+end
+
+get '/secret' do
+  "I have a secret"
+end
+
+get '/home' do
+  "Home page "
+end
+
+get '/about' do
+  "About page"
 end
